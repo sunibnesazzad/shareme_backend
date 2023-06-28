9161a9db78476899ace0180b62dc82325ecaee5d
sunibnesazzad <sunibne.sazzad@gmail.com>
2023-06-28 12:21:42 +0600

    Completing the Backend schemas for Share_Me react project in sanity.io project name navy-duck

---
+++ schemas/pin.js
@@ -0,0 +1,54 @@
+export default {
+    name : 'pin',
+    title : 'Pin',
+    type : 'document',
+    fields : [
+        
+        
+        {
+            name : 'about',
+            title : 'About',
+            type : 'string'
+        },
+        {
+            name : 'destination',
+            title : 'Destination',
+            type : 'url'
+        },
+        {
+            name : 'category',
+            title : 'Category',
+            type : 'string'
+        },
+        {
+            name : 'image',
+            title : 'Image',
+            type : 'image',
+            options : {
+                hotspot : true
+            }
+        },
+        {
+            name : 'userId',
+            title : 'UserId',
+            type : 'string'
+        },
+        {
+            name : 'postedBy',
+            title : 'PostedBy',
+            type : 'postedBy'
+        },
+        {
+            name : 'save',
+            title : 'Save',
+            type : 'array',
+            of : [{ type : 'save' }]
+        },
+        {
+            name : 'comments',
+            title : 'Comments',
+            type : 'array',
+            of : [{ type : 'comment' }]
+        },
+    ]
+}
